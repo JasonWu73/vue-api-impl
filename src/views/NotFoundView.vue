cccaa00cce90b00ac6285d1c67695fd2c7f0f420
吴仙杰 <wuxianjiezh@gmail.com>
2022-09-19 16:22:06 +0800

    增加 fallback 路由

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Page not found</h1>
+    <p>This page could not be found - maybe check out all our<router-link to="/">Home</router-link>.
+    </p>
+  </div>
+</template>
